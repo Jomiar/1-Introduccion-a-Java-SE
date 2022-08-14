522c0bf7f073083e74a12b5752e9076891bff1d1
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-14 07:53:10 -0500

    Se cambio de carpeta al README.md a DataType, tambien se creo la clase DataTypesJava

--- 1. JavaBasico/DataTypesJava/src/DataTypes.java
+++ 1. JavaBasico/DataTypesJava/src/DataTypes.java
@@ -1,5 +1,11 @@
 public class DataTypes {
     public static void main(String[] args) {
+        int n = 1234567890;
+
+        //Se coloca una L al final del número para que los
+        //reconozca como una variable L.
+        long nL = 1234567890L;
+
 
     }
 }
