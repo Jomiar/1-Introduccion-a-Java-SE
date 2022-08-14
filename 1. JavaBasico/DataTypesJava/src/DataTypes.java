4da6419c5efdb1aef428001001a31925f49ec3e5
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-14 08:11:33 -0500

    Tipos de datos char y booblean

--- 1. JavaBasico/DataTypesJava/src/DataTypes.java
+++ 1. JavaBasico/DataTypesJava/src/DataTypes.java
@@ -1,10 +1,21 @@
 public class DataTypes {
     public static void main(String[] args) {
         int n = 1234567890;
-
-        //Se coloca una L al final del número para que los
-        //reconozca como una variable L.
         long nL = 1234567890L;
+
+        double nD = 123.456;
+        float nF = 123.456F;
+
+        var salary = 1000; //int
+        //pension 3%
+        var pension = salary*0.03; //double
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Anahi Salgado";
+        System.out.println("EMPLOYEE: " + employeeName + "SALARY: " + totalSalary);
 
 
     }
