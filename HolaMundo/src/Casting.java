a5a3dbe73228bcda16bd60ec3bac1a67e522e199
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-15 00:04:55 -0500

    Se elimino la carpeta 1. JavaBasico y se traslado el contenido a las carpeta Backend con Java (Beta)

--- HolaMundo/src/Casting.java
+++ HolaMundo/src/Casting.java
@@ -20,5 +20,13 @@ public class Casting {
 
         double c = (double) a/b;
         System.out.println(c);
+
+        char n = '1';
+        int nI = n;
+        System.out.println(nI);
+
+        short nS = (short) n;
+        System.out.println(nS);
+
     }
 }
