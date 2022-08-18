7ac85a3af0e30f658584122db9540c914c594918
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-17 20:47:40 -0500

    Se creo la clase ifStatement y se trabajo la condicion if

---
+++ HolaMundo/src/ifStatement.java
@@ -0,0 +1,13 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        //boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
+        int fileSended = 3;
+
+        if (isBluetoothEnabled){
+            //Send file
+            fileSended++;
+            System.out.println("Arvhivo Enviado");
+        }
+    }
+}
