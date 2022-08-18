ac2c09b7d5e5dd34b4b7faa6417fca186d3b11f9
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-18 10:37:07 -0500

    Se agrego la funcion else

--- HolaMundo/src/ifStatement.java
+++ HolaMundo/src/ifStatement.java
@@ -1,3 +1,5 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class ifStatement {
     public static void main(String[] args) {
         //boolean isBluetoothEnabled = true;
@@ -6,8 +8,19 @@ public class ifStatement {
 
         if (isBluetoothEnabled){
             //Send file
+            //La variable fileSended solo ser usada dentro de la funcion método
             fileSended++;
             System.out.println("Arvhivo Enviado");
+
+            int i = 0;
+            //La variable i++, quiere decir que cada vez que el software recorra
+            //dicha variable, esta se ira aumentando de 1 en 1.
+            i++;
+        } else{
+            fileSended--;
+            System.out.println("Porfavor enciende tu Bloetooth   para iniciar la transferenca");
         }
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);
     }
 }
