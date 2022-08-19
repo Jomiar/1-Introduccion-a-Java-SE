4793d141f6386e8bbc5408000afefee4c6dbf0de
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-18 21:59:39 -0500

    Se creo el JavaDoc a la funcion converterToDolar

--- HolaMundo/src/Funciones.java
+++ HolaMundo/src/Funciones.java
@@ -20,7 +20,10 @@ public class Funciones {
         //(4/3) * pi * r3
         double volumenEsfera = sphereVolumen(y);
         System.out.println(volumenEsfera);
+
+        System.out.println("PESOS A DOLARES: " + converterToDolar(200, "MXN"));
     }
+
 
     public static double circleArea(double r){
         return Math.PI * Math.pow(r, 2);
@@ -31,7 +34,26 @@ public class Funciones {
     }
 
     public static double sphereVolumen(double r){
-        return (4/3) * Math.PI * Math.pow(r,3);
+        return (4/3) * Math.PI *  Math.pow(r,3);
+    }
+
+    /**
+     * Descripcion: Funcion que especificando su moneda convierte una cantidad de dinero a dolares.
+     * @param quantity Cantidad de dinero
+     * @param currency Tipo de moneda: Solo acepta MXN o COP
+     * @return quantity Devuelve cantidad en dolares
+     *  */
+    public static double converterToDolar(double quantity, String currency){
+        //MXN COP
+        switch (currency){
+            case "MXN":
+                quantity = quantity * 0.052;
+                break;
+            case "COP":
+                quantity = quantity * 0.00031;
+                break;
+        }
+        return quantity;
     }
 
 
--- HolaMundo/src/MathematicOperations.java
+++ HolaMundo/src/MathematicOperations.java
@@ -2,11 +2,21 @@ public class MathematicOperations {
     public static void main(String[] args) {
         double x = 2.1;
         double y = 3;
-        System.out.println(Math.ceil(x));//Devuelve un Redondeo hacia arriba
-        System.out.println(Math.floor(x));//Devuelve un Redondeo hacia abajo
-        System.out.println(Math.pow(x,y));//Devuelve un numero elevado al otro
-        System.out.println(Math.max(x,y));//Devuelve el numero mayor
-        System.out.println((int) Math.sqrt(y));//Devuelve la raiz cuadrada
+        //Devuelve un Redondeo hacia arriba
+        System.out.println(Math.ceil(x));
+
+        //Devuelve un Redondeo hacia abajo
+        System.out.println(Math.floor(x));
+
+        //Devuelve un numero elevado al otro
+        System.out.println(Math.pow(x,y));
+
+        //Devuelve el numero mayor
+        System.out.println(Math.max(x,y));
+
+        //Devuelve la raiz cuadrada
+        System.out.println((int) Math.sqrt(y));
+
         //Area de un circulo
         //pi * r2
         System.out.println(Math.PI * Math.pow(y,2));
