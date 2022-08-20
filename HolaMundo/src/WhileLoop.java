1efdce78776d69b4fd4f0189880b7450b6698985
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-20 14:23:14 -0500

    Se creo la clase WhileLoop.java que trata sobre un mensaje SOS, donde se utiliza el bucle While y funciones para definir lo que se va a retornar y lo que se va a imprimir segun introduzca el usuario.

---
+++ HolaMundo/src/WhileLoop.java
@@ -0,0 +1,26 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class WhileLoop {
+
+    static boolean isTurnOnLight = false;
+
+    public static void main(String[] args) {
+
+        turnOnOffLight();
+        int i = 1;
+        while (isTurnOnLight && i <= 10){
+            printSOS();
+            i++;
+        }
+    }
+
+    public static void printSOS(){
+        System.out.println("...___...");
+    }
+
+    //Operador Ternario
+    public static boolean turnOnOffLight(){
+        isTurnOnLight = (isTurnOnLight)?false:true;
+        return isTurnOnLight;
+    }
+}
