02f0064eb6b5797902ca85b53ed9bfe53f41a886
Jose Dario Millan Aranibar <jossedar1990@icloud.com>
2022-08-20 15:57:27 -0500

    Se agrego Versiones de Android y la ubicacion de un changuito en en un arreglo de 4 dimensiones todo los cambios se realizo en la Clase Arrays.java

--- HolaMundo/src/Arrays.java
+++ HolaMundo/src/Arrays.java
@@ -1,3 +1,5 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Arrays {
     public static void main(String[] args) {
         String[] androidVersions = new String[17];
@@ -8,5 +10,38 @@ public class Arrays {
 
         int[][][] numbers = new int[2][2][2];
         int[][][][] numbers4 = new int[2][2][2][2];
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Medellin";
+        cities[1][0] = "Colombia";
+        cities[1][1] = "Bogota";
+        cities[2][0] = "Mexico";
+        cities[2][1] = "Guadalajara";
+        cities[3][0] = "Mexico";
+        cities[3][1] = "CDMX";
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
+
+        System.out.println(animals[1][0][0][1]);
     }
 }
